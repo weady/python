2c34f474de2f0280aeae8f9820856d8af05631be
root <root@linux.(none)>
2016-03-08 09:04:57 -0800

    update

--- python.module.py
+++ python.module.py
@@ -238,5 +238,92 @@ def com_file():
     d = difflib.HtmlDiff()
     print d.make_file(text1_line,text2_line)
 
-com_file()
+#----------------------07------------------------------------
+#filecmp 模块实现文件、目录、遍历子目录的差异对比功能
 
+import filecmp
+
+def com_dir():
+    a = sys.argv[1]	#外部传入的第一个参数，作为左目录        
+    b = sys.argv[2]	#外部传入的第二个参数，作为右目录
+    dirobj=filecmp.dircmp(a,b,['test.py']) 	#目录比较,忽略test.py文件
+
+    #输出对比结果数据报表
+    dirobj.report() #比较当前指定目录中的内容
+    dirobj.report_partial_closure()	#比较当前指定目录及第一子目录中的内容
+    dirobj.report_full_closure()	#递归比较所有指定目录的内容
+
+    print "left_list:" + str(dirobj.left_list)
+    print "right_list:" + str(dirobj.right_list)
+    print "common:" + str(dirobj.common)
+    print "left_only:" + str(dirobj.left_only)
+    print "right_only:" + str(dirobj.right_only)
+    print "common_dirs:" + str(dirobj.common_dirs)	#两边目录都存在的子目录
+    print "common_files:" + str(dirobj.common_files)	#两边目录都存在的子文件
+    print "common_funny:" + str(dirobj.common_funny)	#两边目录都存在的子目录
+    print "same_files:" + str(dirobj.same_files)	#匹配相同的文件
+    print "diff_files:" + str(dirobj.diff_files)	#不匹配的文件
+    print "funny_files:" + str(dirobj.funny_files)	#两边目录中都存在,但无法比较的文件
+
+#校验源与备份目录差异，使用filecmp模块的left_only,diff_files方法递归获取源目录的更新项,再通过shutil.copyfile
+#os.makedirs 方法对更新项进行复制，最终保持一致状态
+#使用到的模块有os,sys filecmp re shutil 
+import re
+import shutil
+holderlist = []
+
+def compare(dir1,dir2):	#递归获取更新项函数
+    dircomp = filecmp.dircmp(dir1,dir2)
+    only_in_src = dircomp.left_only	#源目录新文件或目录
+    diff_in_src = dircomp.diff_files	#不匹配文件,源目录文件已发生变化
+    dirpath = os.path.abspath(dir1)	#定义源目录绝对路径
+    #将更新的文件名或目录追加到holderlist
+    [holderlist.append(os.path.abspath(os.path.join(dir1,x))) for x in only_in_src]    
+    [holderlist.append(os.path.abspath(os.path.join(dir1,x))) for x in diff_in_src]    
+    
+    if len(dircomp.common_dirs) >0:	#判断是否存在相同的字目录，以便递归
+	for item in dircomp.common_dirs:	#递归子目录
+		compare(os.path.abspath(os.path.join(dir1,item)),os.path.abspath(os.path.join(dir2,item)))
+    return holderlist
+
+def compare_main():
+    if len(sys.argv) >2:
+	dir1 = sys.argv[1]
+	dir2 = sys.argv[2]
+    else:
+	print "Usage:",sys.argv[0],"datadir backupdir"
+	sys.exit()
+
+    source_files=compare(dir1,dir2)	#对比源目录和备份目录
+    dir1 = os.path.abspath(dir1)
+
+    if not dir2.endswith('/'):dir2 = dir2+'/'
+    dir2 = os.path.abspath(dir2)
+    destination_files = []
+    createdir_bool = False
+
+    for item in source_files:	#遍历返回的差异文件或目录清单
+	destination_dir = re.sub(dir1,dir2,item)	#将源目录差异路径清单对应替换成备份目录
+
+	destination_files.append(destination_dir)
+	if os.path.isdir(item):
+		if not os.path.exists(destination_dir):
+			os.makedirs(destination_dir)
+			createdir_bool = True
+
+	if createdir_bool:
+		destination_files = []
+		source_files =[]
+		source_files = compare(dir1,dir2)
+		for item in source_files:
+			destination_dir = re.sub(dir1,dir2,item)
+			destination_files.append(destination_dir)
+
+	print "update item:"
+	print source_files	#输出更新列表清单
+	copy_pair = zip(source_files,destination_files)	#将源目录与备份目录文件清单拆分成元组
+	for item in copy_pair:
+		if os.path.isfile(item[0]):	#判断是否为文件,是则进行复制操作
+			shutil.copy2(item[0],item[1])
+
+compare_main()

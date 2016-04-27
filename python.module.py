900e18519afe7f9d3d42b86abbdd8936ae7c5cd0
root <root@linux.(none)>
2016-04-27 09:44:52 -0700

    update

--- python.module.py
+++ python.module.py
@@ -328,4 +328,17 @@ def compare_main():
 
 #----------------------08------------------------------------
 #pycurl 支持ftp,http,https,telnet 类似linux下的curl命令，可以实现探测web服务质量情况
+#----------------------09------------------------------------
+#csv 模块
+import csv
+def csv_mode():
+	rows = [{'Column1': '0', 'Column2': '1', 'Column3': '2', 'Column4': '3'},
+	{'Column1': '0', 'Column2': '1', 'Column3': '2', 'Column4': '3'},
+	{'Column1': '0', 'Column2': '1', 'Column3': '2', 'Column4': '3'},
+	{'Column1': '0', 'Column2': '1', 'Column3': '2', 'Column4': '3'},
+	{'Column1': '0', 'Column2': '1', 'Column3': '2', 'Column4': '3'}]
 
+	fieldnames = ['Column1', 'Column2', 'Column3', 'Column4']
+	dict_writer = csv.DictWriter(open(r'my.csv', 'wb'), fieldnames=fieldnames)
+	dict_writer.writeheader() #写入首行
+	dict_writer.writerows(rows)

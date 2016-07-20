b18ee4618ee460a7ff8efd626f975ee189fd1924
weady <708964732@qq.com>
2016-07-20 19:47:39 +0800

    delete

--- Django/website/monitor/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from monitor.models import *
-
-# Register your models here.
-admin.site.register(AuthUser)
--- Django/website/monitor/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import patterns,include,url
-from monitor.views import *
-
-urlpatterns = patterns('monitor.views',
-	url(r'^$','login'),
-	url(r'^index/$','index'),
-	url(r'^host/$','hosts'),
-	url(r'^logout/$','logout'),
-)

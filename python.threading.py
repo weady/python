1d16b1a52b3f2bfcefffeb0f7e3bf837ad7f5034
weady <708964732@qq.com>
2016-08-30 09:55:45 +0800

    update

--- python.threading.py
+++ python.threading.py
@@ -7,10 +7,11 @@
 
 from atexit import register
 from random import randrange
-from threading import Thread,Lock,currentThread
+import threading
 from time import sleep,ctime
 
 
+#-----------------------------------------------------------------
 #定义了一个类实现对threading模块的thread方法进行重写
 class MyThread(threading.Thread):
     def __init__(self,func,args,name=''):
@@ -27,11 +28,12 @@ class MyThread(threading.Thread):
         self.res = self.func(*self.args)
         print self.name,'Finished at:',ctime()
 
+#-----------------------------------------------------------------
 class CleanOutputSet(set):
 	def __str__(self):
 		return ','.join(x for x in self)
 
-lock = Lock()
+lock = threading.Lock()
 loops = (randrange(2,5) for x in xrange(randrange(3,7)))
 remaining = CleanOutputSet()
 
@@ -65,9 +67,56 @@ def main():
 	for pause in loops:
 		Thread(target=loop1,args=(pause,)).start()
 
+#使用atexit模块中的register方法,以便让解释器在脚本退出前执行该函数,无论改函数位于何处
 @register
 def _atexit():
 	print 'ALL Done at:',ctime()
 
-if __name__ == '__main__':
-	main()
+#-----------------------------------------------------------------
+#生产者消费者模型
+from random import randint
+from time import sleep
+from Queue import Queue
+
+def writeQ(queue):
+	print 'Producing object for Q....',
+	queue.put('xxx',1)
+	print 'size now',queue.qsize()
+
+def readQ(queue):
+	val = queue.get(1)
+	print 'consumed object from Q.... size now',queue.qsize()
+
+def writer(queue,loops):
+	for i in range(loops):
+		writeQ(queue)
+		sleep(randint(1,3))
+
+def reader(queue,loops):
+	for i in range(loops):
+		readQ(queue)
+		sleep(randint(2,5))
+
+funcs = [writer,reader]
+nfuncs = range(len(funcs))
+
+def main01():
+	nloops = randint(2,5)
+	q = Queue(32)
+
+	threads = []
+	for i in nfuncs:
+		t = MyThread(funcs[i],(q,nloops),funcs[i].__name__)
+		threads.append(t)
+
+	for i in nfuncs:
+		threads[i].start()
+
+	for i in nfuncs:
+		threads[i].join()
+
+	print 'all Done'
+
+main01()
+
+

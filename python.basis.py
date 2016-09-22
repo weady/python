f21ae42480f87b2ca12b7ab44a8a9c9d6a9002d3
weady <708964732@qq.com>
2016-09-22 10:20:27 +0800

    update

--- python.basis.py
+++ python.basis.py
@@ -138,5 +138,34 @@ def downloadfile():
 
 	# urllib.urlretrieve(url, "demo.zip")
 
-	
+##############################################################################
+#经典的冒泡 选择 插入排序算法
+def bubble_sort(seq):
+    for i in range(len(seq)):
+        for j in range(i,len(seq)):
+            if seq[j] < seq[i]:
+                tmp = seq[j]
+                seq[j] = seq[i]
+                seq[i] = tmp
+                  
+def selection_sort(seq):
+    for i in range(len(seq)):
+        position = i
+        for j in range(i,len(seq)):
+            if seq[position] > seq[j]:
+                position = j
+        if position != i:
+                tmp = seq[position]
+                seq[position] = seq[i]
+                seq[i] = tmp
+  
+def insertion_sort(seq):
+    if len(seq) > 1:
+        for i in range(1,len(seq)):
+            while i > 0 and seq[i] < seq[i-1]:
+                tmp = seq[i]
+                seq[i] = seq[i-1]
+                seq[i-1] = tmp
+                i = i - 1
+
 
